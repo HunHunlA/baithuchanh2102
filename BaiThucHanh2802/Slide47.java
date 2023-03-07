68a5b1db5c33b71cd40bd630f30551c4e22761c8
HunHunlA <nguyenminhhienmx@gmail.com>
2023-03-07 11:48:59 +0700

    updatecode

---
+++ BaiThucHanh2802/Slide47.java
@@ -0,0 +1,19 @@
+    import java.util.Scanner;
+    public class Slide47 {
+    
+
+    public static void main(String[] args){
+        int number, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        
+         while (sum=100)
+         
+        {System.out.println("Nhap so nguyen duong bat ki: ");
+          number = sc.nextInt();  
+        sum += number;
+        }
+        
+        System.out.println("Tong cac so nguyen duong vua nhap: " +sum);
+
+        }
+}
